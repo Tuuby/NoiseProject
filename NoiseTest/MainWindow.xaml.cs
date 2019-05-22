72e12a3478982d40f95d3131feb8121f0914038e
Tobias Trompell <tobiastrompell@aol.com>
2019-05-22 11:27:37 +0200

    First Commmit

---
+++ NoiseTest/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Windows;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace NoiseTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Bitmap map2 = new Bitmap(500, 500);
+        
+        Color col = new Color();
+        Random rdm = new Random();
+        public MainWindow()
+        {
+            InitializeComponent();
+            drawMap();
+        }
+
+        private void drawMap()
+        {
+            int seed = rdm.Next();
+            Map map = new Map(500, 500, seed);
+            map.setScale(5f);
+            map.setWaterlevel(65);
+
+            map.Generate();
+            for (int x = 0; x < map2.Width; x++)
+            {
+                for (int y = 0; y < map2.Height; y++)
+                {
+                    byte elevation = map.getElevation(x, y);
+                    if (map.getElevation(x, y) <= map.getWaterlevel())
+                    {
+                        col = Color.FromArgb(255, 0, 0, 100);
+                    } else
+                    {
+                        col = Color.FromArgb(255, (byte)(32 + elevation / 1.14), (byte)(32 + elevation / 1.14), (byte)(32 + elevation / 1.14));
+                    }
+                    map2.SetPixel(x, y, col);
+                }
+            }
+            FileStream fs = new FileStream(@"C:\Users\phant\Desktop\map.png", FileMode.Create);
+            map2.Save(fs, ImageFormat.Png);
+        }
+
+        private void SeedRoll_Click(object sender, RoutedEventArgs e)
+        {
+            //MapCanvas.Children.Clear();
+            drawMap();
+        }
+    }
+}
---
+++ NoiseTest/Map.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoiseTest
+{
+    class Map
+    {
+        private int width = 10;
+        private int height = 10;
+        private byte[,] elevation;
+        private float scale = 0.01f;
+        private byte waterlevel =  100;
+
+        public Map(int width, int height, int seed)
+        {
+            this.width = width;
+            this.height = height;
+            elevation = createArray();
+            Noise.Seed = seed;
+        }
+
+        public Map(int width, int height)
+        {
+            this.width = width;
+            this.height = height;
+            elevation = createArray();
+        }
+
+        public Map(int seed)
+        {
+            elevation = createArray();
+            Noise.Seed = seed;
+        }
+
+        public Map()
+        {
+            elevation = createArray();
+        }
+
+        private byte[,] createArray()
+        {
+            return new byte[width, height];
+        }
+
+        public void setSeed(int Seed)
+        {
+            Noise.Seed = Seed;
+        }
+
+        public byte getElevation(int x, int y)
+        {
+            return elevation[x, y];
+        }
+
+        public byte[,] getAll()
+        {
+            return elevation;
+        }
+
+         public void setScale(float scale)
+        {
+            if ((scale >= 1) && (scale <= 5)) {
+                this.scale = scale / 500;
+            } else
+            {
+                throw new ArgumentException("Falscher Parameter du Affe");
+            }
+        }
+
+        public float getScale()
+        {
+            return scale;
+        }
+
+        public void setWaterlevel(byte level)
+        {
+            waterlevel = level;
+        }
+
+        public byte getWaterlevel()
+        {
+            return waterlevel;
+        }
+
+        public void Generate()
+        {
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    double el = (Noise.CalcPixel2D(x, y, 0.5f * scale)
+                                    + 0.5 * Noise.CalcPixel2D(x, y, 2 * scale)
+                                    + 0.25 * Noise.CalcPixel2D(x, y, 4 * scale)) / 1.75;    //1.75 ist wichtig um innerhalb der Grenzen eines Bytes zu bleiben
+                    elevation[x, y] = (byte) (Math.Pow(el, 2) / 255);
+                }
+            }
+        }
+    }
+}
